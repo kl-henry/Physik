1f397c77ad2b095fee7a05a0e301fa5506f0489c
hhill <hz.hill@web.de>
2022-04-12 13:43:50 +0200

    inkl. Menü Differntialgl.

--- vector_rechnung/angle_Vectors.py
+++ vector_rechnung/angle_Vectors.py
@@ -5,11 +5,11 @@ from vector_rechnung.mag_vector import magVectors
 
 
 def angleBetween(v1, v2):
-    print("in function angleBetween: ", v1, v2)
+    # print("in function angleBetween: ", v1, v2)
     SkalarProkdukt = v1[0]*v2[0] + v1[1]*v2[1]
     Nenner = magVectors(v1)*magVectors(v2)
 
-    print(f"angleBetween: Terme: SkalarProkdukt={SkalarProkdukt:.3f}, Nenner={Nenner:.3f}")
+    # print(f"angleBetween: Terme: SkalarProkdukt={SkalarProkdukt:.3f}, Nenner={Nenner:.3f}")
 
     return math.degrees(math.acos(SkalarProkdukt / Nenner))
 
